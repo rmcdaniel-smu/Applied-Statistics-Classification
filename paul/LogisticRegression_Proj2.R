b4ea99a1efd6cbfe6c95d8abc31362aa54b0b67e
PaulAdams4361 <paula@smu.edu>
2019-11-21 23:03:02 -0600

    Updating RMD

--- paul/LogisticRegression_Proj2.R
+++ paul/LogisticRegression_Proj2.R
@@ -1,10 +1,56 @@
 library(pacman)
 p_load(ggplot2, dplyr, tidyverse, dummies, MASS)
 
-df <- read.csv("C:/Users/Pablo/Desktop/KG6372/paul/modelingKobeData.csv", 
+df <- read.csv("./modelingKobeData.csv", 
                header=T, sep=",", strip.white=T, stringsAsFactors = F, na.strings=c(""))
 
+<<<<<<< HEAD
 
+=======
+df[which(df$action_type == "Alley Oop Dunk Shot"),2] = "short"
+df[which(df$action_type == "Cutting Layup Shot"),2] = "short"
+df[which(df$action_type == "Driving Dunk Shot"),2] = "short"
+df[which(df$action_type == "Driving Finger Roll Layup Shot"),2] = "short"
+df[which(df$action_type == "Driving Layup Shot"),2] = "short"
+df[which(df$action_type == "Driving Reverse Layup Shot"),2] = "short"
+df[which(df$action_type == "Driving Slam Dunk Shot"),2] = "short"
+df[which(df$action_type == "Dunk Shot"),2] = "short"
+df[which(df$action_type == "Finger Roll Layup Shot"),2] = "short"
+df[which(df$action_type == "Follow Up Dunk Shot"),2] = "short"
+df[which(df$action_type == "Layup Shot"),2] = "short"
+df[which(df$action_type == "Putback Dunk Shot"),2] = "short"
+df[which(df$action_type == "Putback Layup Shot"),2] = "short"
+df[which(df$action_type == "Putback Slam Dunk Shot"),2] = "short"
+df[which(df$action_type == "Reverse Dunk Shot"),2] = "short"
+df[which(df$action_type == "Reverse Layup Shot"),2] = "short"
+df[which(df$action_type == "Reverse Slam Dunk Shot"),2] = "short"
+df[which(df$action_type == "Running Dunk Shot"),2] = "short"
+df[which(df$action_type == "Running Finger Roll Layup Shot"),2] = "short"
+df[which(df$action_type == "Running Layup Shot"),2] = "short"
+df[which(df$action_type == "Running Reverse Layup Shot"),2] = "short"
+df[which(df$action_type == "Running Slam Dunk Shot"),2] = "short"
+df[which(df$action_type == "Running Tip Shot"),2] = "short"
+df[which(df$action_type == "Slam Dunk Shot"),2] = "short"
+df[which(df$action_type == "Tip Layup Shot"),2] = "short"
+df[which(df$action_type == "Tip Shot"),2] = "short"
+df$action_type <- ifelse(df$action_type=="short", "short", "long")
+
+df[which(df$combined_shot_type == "Jump Shot"),2] = "short"
+df[which(df$combined_shot_type == "Dunk"),2] = "short"
+df[which(df$combined_shot_type == "Layup"),2] = "short"
+df[which(df$combined_shot_type == "Tip Shot"),2] = "short"
+df[which(df$combined_shot_type == "Hook Shot"),2] = "short"
+df[which(df$combined_shot_type == "Bank Shot"),2] = "short"
+df$combined_shot_type <- ifelse(df$combined_shot_type=="short", "short", "long")
+
+df[which(df$loc_y > 300),17] <- "3PT Field Goal"
+df$shot_type <- ifelse(df$shot_type=="2PT Field Goal", 2, 3)
+
+#remove one-level factors, 
+df <- df %>% subset(select=-c(team_id, team_name, season, shot_zone_area, shot_zone_basic, shot_zone_range, matchup))
+df <- df %>% subset(select=-c(team_id, team_name))
+df <- df %>% mutate_if(is.integer, as.numeric) %>% mutate_if(is.character, as.factor) %>% data.frame()
+>>>>>>> 498358101e7aea8b713d51270693b6d64122c1e4
 
 ########################## fiele x dummy variable creation##########
 #x <- data.frame(df$x)
